79c9a936cd5df5601544246cc7dfd5b5c38f60f8
Jack Willson <jwi182@uclive.ac.nz>
2024-03-21 12:37:10 +1300

    new files

---
+++ ADC.h
@@ -0,0 +1,31 @@
+/*
+ * ADC.h
+ *
+ *  Created on: 21/03/2024
+ *      Author: jwi182
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/adc.h"
+#include "driverlib/pwm.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/debug.h"
+#include "utils/ustdlib.h"
+#include "circBufT.h"
+#include "OrbitOLED/OrbitOLEDInterface.h"
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+
+
+
+
+#endif /* ADC_H_ */
