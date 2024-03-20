5350e7c91fbf03a9c75bad4aeb46ff8e59eb5311
hrc48 <hrc48@uclive.ac.nz>
2024-03-21 12:54:38 +1300

    updated milestone1.c

--- milestone1.c
+++ milestone1.c
@@ -2,9 +2,102 @@
  * milestone1.c
  *
  *  Created on: 21/03/2024
- *      Author: jwi182
+ *      Authors: jwi182, hrc48
  */
 
 
 
+//*****************************************************************************
+//
+// The interrupt handler for the for SysTick interrupt.
+//
+//*****************************************************************************
+void
+SysTickIntHandler(void)
+{
+    //
+    // Initiate a conversion
+    //
+    ADCProcessorTrigger(ADC0_BASE, 3);
+    g_ulSampCnt++;
+}
+
+//*****************************************************************************
+//
+// The handler for the ADC conversion complete interrupt.
+// Writes to the circular buffer.
+//
+//*****************************************************************************
+void
+ADCIntHandler(void)
+{
+    uint32_t ulValue;
+
+    //
+    // Get the single sample from ADC0.  ADC_BASE is defined in
+    // inc/hw_memmap.h
+    ADCSequenceDataGet(ADC0_BASE, 3, &ulValue);
+    //
+    // Place it in the circular buffer (advancing write index)
+    writeCircBuf (&g_inBuffer, ulValue);
+    //
+    // Clean up, clearing the interrupt
+    ADCIntClear(ADC0_BASE, 3);
+}
+
+//*****************************************************************************
+// Initialisation functions for the clock (incl. SysTick), ADC, display
+//*****************************************************************************
+void
+initClock (void)
+{
+    // Set the clock rate to 20 MHz
+    SysCtlClockSet (SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ);
+    //
+    // Set up the period for the SysTick timer.  The SysTick timer period is
+    // set as a function of the system clock.
+    SysTickPeriodSet(SysCtlClockGet() / SAMPLE_RATE_HZ);
+    //
+    // Register the interrupt handler
+    SysTickIntRegister(SysTickIntHandler);
+    //
+    // Enable interrupt and device
+    SysTickIntEnable();
+    SysTickEnable();
+}
+
+
+
+
+
+
+void
+initDisplay (void)
+{
+    // intialise the Orbit OLED display
+    OLEDInitialise ();
+}
+
+//*****************************************************************************
+//
+// Function to display the mean ADC value (10-bit value, note) and sample count.
+//
+//*****************************************************************************
+void
+displayMeanVal(uint16_t meanVal, uint32_t count)
+{
+    char string[17];  // 16 characters across the display
+
+    OLEDStringDraw ("ADC demo 1", 0, 0);
+
+    // Form a new string for the line.  The maximum width specified for the
+    //  number field ensures it is displayed right justified.
+    usnprintf (string, sizeof(string), "Mean ADC = %4d", meanVal);
+    // Update line on display.
+    OLEDStringDraw (string, 0, 1);
+
+    usnprintf (string, sizeof(string), "Sample # %5d", count);
+    OLEDStringDraw (string, 0, 3);
+}
 
