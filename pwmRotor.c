f2ba42c17eb5a81c8a6f2408d42231e46a5732dc
hrc48 <hrc48@uclive.ac.nz>
2024-05-07 16:03:48 +1200

    start creating pwm files and halfway through systick schedulling

---
+++ pwmRotor.c
@@ -0,0 +1,87 @@
+/*
+ * pwmRotor.c
+ *
+ *  Created on: 7/05/2024
+ *      Author: hrc48
+ */
+
+#include <pwmRotor.h>
+
+/*********************************************************
+ * initialisePWM
+ * M0PWM7 (J4-05, PC5) is used for the main rotor motor
+ * M1PWM5 (J3, PF1) is used for the tail rotor motor
+ *********************************************************/
+void
+initialisePWM (void)
+{
+    //init main
+    SysCtlPeripheralEnable(PWM_MAIN_PERIPH_PWM);
+    SysCtlPeripheralEnable(PWM_MAIN_PERIPH_GPIO);
+
+    GPIOPinConfigure(PWM_MAIN_GPIO_CONFIG);
+    GPIOPinTypePWM(PWM_MAIN_GPIO_BASE, PWM_MAIN_GPIO_PIN);
+
+    PWMGenConfigure(PWM_MAIN_BASE, PWM_MAIN_GEN,
+                    PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
+    // Set the initial PWM parameters
+    setPWM (PWM_START_RATE_HZ, PWM_START_DUTY);
+
+    PWMGenEnable(PWM_MAIN_BASE, PWM_MAIN_GEN);
+
+    // Disable the output.  Repeat this call with 'true' to turn O/P on.
+    PWMOutputState(PWM_MAIN_BASE, PWM_MAIN_OUTBIT, false);
+
+
+    //init Tail
+    SysCtlPeripheralEnable(PWM_TAIL_PERIPH_PWM);
+    SysCtlPeripheralEnable(PWM_TAIL_PERIPH_GPIO);
+
+    GPIOPinConfigure(PWM_TAIL_GPIO_CONFIG);
+    GPIOPinTypePWM(PWM_TAIL_GPIO_BASE, PWM_TAIL_GPIO_PIN);
+
+    PWMGenConfigure(PWM_TAIL_BASE, PWM_TAIL_GEN,
+                    PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
+    // Set the initial PWM parameters
+    // Calculate the PWM period corresponding to the freq.
+    uint32_t ui32Period =
+        SysCtlClockGet() / PWM_DIVIDER / PWM_TAIL_FREQ;
+
+    PWMGenPeriodSet(PWM_TAIL_BASE, PWM_TAIL_GEN, ui32Period);
+    PWMPulseWidthSet(PWM_TAIL_BASE, PWM_TAIL_OUTNUM,
+        ui32Period * PWM_TAIL_DUTY / 100);
+
+
+    PWMGenEnable(PWM_TAIL_BASE, PWM_TAIL_GEN);
+
+    // Disable the output.  Repeat this call with 'true' to turn O/P on.
+    PWMOutputState(PWM_TAIL_BASE, PWM_TAIL_OUTBIT, false);
+}
+
+
+/********************************************************
+ * Function to set the freq, duty cycle of M0PWM7
+ ********************************************************/
+void
+setPWM (uint32_t ui32Freq, uint32_t ui32Duty)
+{
+    // Calculate the PWM period corresponding to the freq.
+    uint32_t ui32Period =
+        SysCtlClockGet() / PWM_DIVIDER / ui32Freq;
+
+    PWMGenPeriodSet(PWM_MAIN_BASE, PWM_MAIN_GEN, ui32Period);
+    PWMPulseWidthSet(PWM_MAIN_BASE, PWM_MAIN_OUTNUM,
+        ui32Period * ui32Duty / 100);
+}
+
+
+
+
+
+
+
+
+
+
+
+
