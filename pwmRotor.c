da968cff1263d47b382572629d316e42024cdc03
Jack Willson <jwi182@uclive.ac.nz>
2024-05-14 17:59:46 +1200

    tidy code up, nice comments

--- pwmRotor.c
+++ pwmRotor.c
@@ -7,12 +7,13 @@
 
 #include <pwmRotor.h>
 
-
+//Heli controller set points
 static int16_t altSetPoint = 0;
 static int16_t yawSetPoint = -223;
 
-static uint16_t MAX_ALT = 0; //since they're constants, they shouldn't be uppercase...
-static uint16_t MIN_ALT = 0;
+//Altitude ADC Max and Min 
+static uint16_t max_alt = 0; 
+static uint16_t min_alt = 0;
 
 
 
@@ -71,26 +72,25 @@ initialisePWM (void)
     PWMOutputState(PWM_TAIL_BASE, PWM_TAIL_OUTBIT, false);
 }
 
-
+//Initialise min and max ADC heights
 void initAltLimits (uint16_t initLandedADC) {
-    //Initialise min and max ADC heights
-    MIN_ALT = initLandedADC;
-    MAX_ALT = initLandedADC - ADC_STEP_FOR_1V;
+    min_alt = initLandedADC;
+    max_alt = initLandedADC - ADC_STEP_FOR_1V;
     altSetPoint = initLandedADC;
 }
 
 
-/********************************************************
- * Function to set the freq, duty cycle of M0PWM7
- ********************************************************/
+
+//Function to set the Main and Tail duty cycles
 void
 setDuty (uint32_t mainDuty, uint32_t tailDuty)
 {
-    //System clock
+    //Get System clock value once, no point running function all the time
     static uint32_t sysClock = 0;
     if (sysClock == 0) {
         sysClock = SysCtlClockGet();
     }
+
     // Calculate the PWM period corresponding to the freq.
     uint32_t ui32PeriodMain = sysClock / PWM_DIVIDER / PWM_MAIN_FREQ;
 
@@ -99,17 +99,13 @@ setDuty (uint32_t mainDuty, uint32_t tailDuty)
     uint32_t ui32PeriodTail = sysClock / PWM_DIVIDER / PWM_TAIL_FREQ;
 
     PWMPulseWidthSet(PWM_TAIL_BASE, PWM_TAIL_OUTNUM, ui32PeriodTail * tailDuty / 100);
-
 }
 
 
-/********************************************************
- * PID controller function for main rotor, returns a duty cycle %
- ********************************************************/
-
+//PID controller function for main rotor, returns a duty cycle %
 int32_t
 controllerMain (uint16_t sensor) {
-
+    //Record integral and prev sensor value
     static float dI = 0;
     static uint16_t prevSensor = 0;
 
@@ -118,14 +114,16 @@ controllerMain (uint16_t sensor) {
     float I = KIM * error * DELTA_T;
     float D = KDM * (prevSensor - sensor) / DELTA_T;
 
-
+    //Gravity is constant offset, controller is negative as ADC is opposite to height
     int32_t control = GRAVITY - P - (dI + I) - D;
 
+    //Cap controller output
     if (control > PWM_DUTY_MAIN_MAX) {
         control = PWM_DUTY_MAIN_MAX;
     } else if (control < PWM_DUTY_MAIN_MIN) {
         control = PWM_DUTY_MAIN_MIN;
     } else {
+        //Accumulate integral
         I = I + dI;
     }
     prevSensor = sensor;
@@ -133,39 +131,42 @@ controllerMain (uint16_t sensor) {
 }
 
 
-/********************************************************
- * PID controller function for tail rotor, returns a duty cycle %
- ********************************************************/
-
+//PID controller function for tail rotor, returns a duty cycle %
 int32_t
 controllerTail (int32_t mainControl, int16_t sensor, bool sweepEn) {
     int16_t error = yawSetPoint - sensor;
-
+    
+    //Turn off controller wrap around at 180 deg when in take off sweeping mode so sweep can complete 360 turn
     if (!sweepEn) {
+        //Adjust error values when crossing 180 deg mark
         if (error < -YAW_ERROR_LIMIT) {
             error = YAW_REV + error;
         } else if (error > YAW_ERROR_LIMIT) {
             error = -YAW_REV + error;
         }
     }
-
+    
+    //Record integral and prev sensor value
     static float dI = 0;
     static int16_t prevSensor = 0;
 
-
+    //PID controller calc
     float P = KPT * error;
     float I = KIT * error * DELTA_T;
     float D = KDT * (prevSensor - sensor) / DELTA_T;
     float PID_TAIL = P + I + D;
 
+    //Limit PID effort without limiting coupling
     if (PID_TAIL > PID_TAIL_MAX) {
         PID_TAIL = PID_TAIL_MAX;
     }
 
+    //Couple tail rotor to main rotor duty
     float coupling = mainControl * KC;
 
     int32_t control = PID_TAIL + coupling;
 
+    //Cap controller output
     if (control > PWM_DUTY_TAIL_MAX) {
         control = PWM_DUTY_TAIL_MAX;
     } else if (control < PWM_DUTY_TAIL_MIN) {
@@ -178,62 +179,68 @@ controllerTail (int32_t mainControl, int16_t sensor, bool sweepEn) {
     return control;
 }
 
-
+//Increase altitude setpoint
 void incAlt (void) {
     altSetPoint -= ALT_STEP;
-    if (altSetPoint < MAX_ALT){
-        altSetPoint = MAX_ALT;
+    if (altSetPoint < max_alt){
+        altSetPoint = max_alt;
     }
 }
 
-
+//Decrease altitude setpoint
 void decAlt (void) {
     altSetPoint += ALT_STEP;
-    if (altSetPoint > MIN_ALT - ALT_STEP){
-        altSetPoint = MIN_ALT - ALT_STEP; //Set min button altitude to 10%
+    if (altSetPoint > min_alt - ALT_STEP){
+        altSetPoint = min_alt - ALT_STEP; //Set min button altitude to 10%
     }
 }
 
+//Set altitude setpoint
 void setAlt (int16_t setPoint) {
     altSetPoint = setPoint;
 }
 
+//Increase yaw setpoint
 void incYaw (void) {
     yawSetPoint += YAW_STEP;
-    //special case
+    //special case - wrap around at 180 deg
     if (yawSetPoint > YAW_ERROR_LIMIT) {
         yawSetPoint = -YAW_ERROR_LIMIT + (yawSetPoint - YAW_ERROR_LIMIT);
     }
 }
 
-
+//Decrease yaw setpoint
 void decYaw (void) {
     yawSetPoint -= YAW_STEP;
-    //special case
+    //special case - wrap around at 180 deg
     if (yawSetPoint < -YAW_ERROR_LIMIT) {
         yawSetPoint = YAW_ERROR_LIMIT + (yawSetPoint + YAW_ERROR_LIMIT);
     }
 }
 
+//Set yaw setpoint
 void setYaw (int16_t setPoint) {
     yawSetPoint = setPoint;
 }
 
+//Get altitude setpoint
 int32_t getAltSet (void) {
     return altSetPoint;
 }
 
+//Get yaw setpoint
 int32_t getYawSet (void) {
     return yawSetPoint;
 }
 
-uint16_t getMIN_ALT (void) {
-    return MIN_ALT;
+//Get min alt setpoint
+uint16_t getmin_alt (void) {
+    return min_alt;
 }
 
-
-uint16_t getMAX_ALT (void) {
-    return MAX_ALT;
+//Get max alt setpoint
+uint16_t getmax_alt (void) {
+    return max_alt;
 }
 
 
